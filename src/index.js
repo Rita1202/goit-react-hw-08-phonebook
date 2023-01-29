a29cfc207636389a4a64a8ffc1b91404b7b46ac4
Rita1202 <Rita.sokolchenko@gmail.com>
2023-01-29 16:08:38 +0100

    main

--- src/index.js
+++ src/index.js
@@ -13,12 +13,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/goit-react-hw-08-movies">
           <App />
         </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
 );
-
-//  basename = '/goit-react-hw-08-movies';
