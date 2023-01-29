4f72158f5f4cf6d08ed5dd5ba12d920b9be49e9d
Rita1202 <Rita.sokolchenko@gmail.com>
2023-01-29 16:20:58 +0100

    main

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import { store } from './redux/store';
-import { persistor } from './redux/store';
+import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-// import './services/eventApi';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
+      <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter basename="/goit-react-hw-08-movies">
           <App />
         </BrowserRouter>
