4dac157fead38f9f5131723c79d4d71202f8e3b8
Rita1202 <Rita.sokolchenko@gmail.com>
2023-01-29 16:13:17 +0100

    main

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import './index.css';
 import { store } from './redux/store';
 import { persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import './services/eventApi';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
